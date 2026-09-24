78d5bd8ce2a14c6c964fc9171900c5d22c68c12a
Speedysautobody <Speedysbodyshop18@icloud.com>
2026-09-24 11:53:51 -0500

    Implement service worker for offline caching

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "speedys-auto-body-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./Speedys.png",
+  "./plantilla.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Instalar Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// Activar y eliminar caches anteriores
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Permitir funcionamiento offline
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseCopy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((cachedResponse) => {
+          return cachedResponse || caches.match("./");
+        });
+      })
+  );
+});
